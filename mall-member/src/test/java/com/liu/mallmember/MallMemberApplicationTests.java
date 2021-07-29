46dda7b2444cd1e567d92ac421a382d993e0464c
Liu <504588684@qq.com>
2021-07-29 18:15:56 +0800

    first commit

---
+++ mall-member/src/test/java/com/liu/mallmember/MallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liu.mallmember;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
